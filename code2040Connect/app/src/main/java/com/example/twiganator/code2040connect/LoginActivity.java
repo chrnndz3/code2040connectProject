e9f67a4a1c35fcf686abecf053e6f1b1ef44ff10
Sebastian Cevallos <scevallos@atlassian.com>
2017-07-15 21:46:18 -0700

    added register stuff

---
+++ code2040Connect/app/src/main/java/com/example/twiganator/code2040connect/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.twiganator.code2040connect;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_layout);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        final Button bLogin = (Button) findViewById(R.id.sign_in_button);
+        final TextView registerLink = (TextView) findViewById(R.id.sign_up_button);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+    }
+}
